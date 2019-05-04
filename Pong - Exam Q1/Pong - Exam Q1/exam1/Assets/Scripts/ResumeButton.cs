576b4a7bd58feaa8d61161d5a840e64ae448b62b
Nikhil Bharadwaj <44049892+pixelbrahma@users.noreply.github.com>
2019-05-04 13:04:12 +0530

    adds

---
+++ Pong - Exam Q1/Pong - Exam Q1/exam1/Assets/Scripts/ResumeButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeButton : MonoBehaviour {
+
+    [SerializeField] private GameObject pause;
+	public void Resume()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
